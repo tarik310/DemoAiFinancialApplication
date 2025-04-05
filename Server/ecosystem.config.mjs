09ef0c3a61750de89cd5787e3789234fc54088d0
tarik310 <tareq310harh@gmail.com>
2025-04-05 15:27:55 +0300

    feat: add ecosystem.config.mjs for application configuration

--- Server/ecosystem.config.mjs
+++ Server/ecosystem.config.mjs
@@ -4,6 +4,7 @@ export default {
       name: "server-fingenius-ai",
       script: "npm",
       args: "run start",
+      interpreter: "bash",
       env: {
         NODE_ENV: "development",
         ENV_VAR1: "environment-variable",
